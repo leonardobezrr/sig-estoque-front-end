8a28651656193cca5c51ac89ab42f4f95032412f
Breno Porfirio <brenoporfirio03@gmail.com>
2024-08-03 17:05:43 -0300

    feat: create user
    Co-authored-by: Ricardo Alencar <ricsjs@users.noreply.github.com>

--- src/pages/manager/index.tsx
+++ src/pages/manager/index.tsx
@@ -1,17 +1,31 @@
+// ManagerUsers.tsx
 import React from "react";
 import ManagerNavbar from "../components/manager-navbar";
 import TableUsers from "../components/manager-table";
 import { AddUserButton, Container } from "@/styles/pages/manager";
 import { DefaultButton } from "@/styles/pages/home";
+import { useModal, ModalProvider } from '../components/create-user-modal/modal-context';
+import CreateUserModal from '../components/create-user-modal/index';
 
-export default function ManagerUsers() {
+const ManagerUsers: React.FC = () => {
+  const { openModal } = useModal();
+
   return (
     <Container>
       <ManagerNavbar />
       <AddUserButton>
-        <DefaultButton>Adicionar Usuário</DefaultButton>
+        <DefaultButton onClick={openModal}>Adicionar Usuário</DefaultButton>
       </AddUserButton>
       <TableUsers />
+      <CreateUserModal />
     </Container>
+  );
+};
+
+export default function ManagerUsersWithProvider() {
+  return (
+    <ModalProvider>
+      <ManagerUsers />
+    </ModalProvider>
   );
 }
--- styles/pages/manager.ts
+++ styles/pages/manager.ts
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import Select from '@mui/material/Select';
 
 export const Container = styled.div`
   display: flex;
@@ -11,4 +12,82 @@ export const Container = styled.div`
 export const AddUserButton = styled.div`
   width: 100%;
   padding: 2rem 0rem 2rem 0rem;
+`;
+
+export const Form = styled.form`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const StyledInput = styled.input`
+  margin-top: 0.4rem;
+  margin-bottom: 0.4rem;
+  width: 20rem;
+  padding: 0.5rem;
+  border: none;
+  border-radius: 0.2rem;
+  outline: none;
+
+  &:focus {
+    border: 1.5px solid #522d80;
+  }
+`;
+
+export const Icon = styled.p`
+  margin-top: 0.3rem;
+  display: flex;
+  margin-bottom: 0.3rem;
+  align-items: center;
+  color: #fff;
+
+  & > svg {
+    margin-right: 0.5rem;
+  }
+`;
+
+export const Text = styled.p`
+  margin-top: 1rem;
+  line-height: 100%;
+  display: flex;
+  justify-content: center;
+  color: white;
+`;
+
+export const DefaultButton = styled.button`
+  all: unset;
+  display: flex;
+  margin-top: 0.3rem;
+  margin-bottom: 0.3rem;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  cursor: pointer;
+  border-radius: 8px;
+  padding: 0.8em;
+  font-weight: 100;
+  font-size: 16px;
+  font-weight: 400;
+  background-color: #522d80;
+  opacity: 0.8;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const StyledSelect = styled(Select)`
+  .MuiSelect-select {
+    color: white; 
+  }
+`;
+
+export const StyledSelectContainer = styled.div`
+  margin-top: 0.4rem;
+  margin-bottom: 0.4rem;
+  width: 20rem;
+  padding: 0.5rem;
+  border-radius: 0.2rem;
+  border: none;
 `;
