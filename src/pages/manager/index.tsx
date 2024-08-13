a8a737ac414b66cd1a811d974c646f632e6b83c3
Lima404 <gabriel.lima2307@uol.com.br>
2024-08-13 13:28:31 -0300

    feat: correctly conflicts

--- src/pages/manager/index.tsx
+++ src/pages/manager/index.tsx
@@ -4,16 +4,8 @@ import ManagerNavbar from "../components/manager/manager-navbar";
 import TableUsers from "../components/manager/manager-table";
 import { AddUserButton, Container } from "@/styles/pages/manager";
 import { DefaultButton } from "@/styles/pages/home";
-<<<<<<< HEAD
 import { useModal, ModalProvider } from '../components/create-user-modal/modal-context';
 import CreateUserModal from '../components/create-user-modal/index';
-=======
-import {
-  useModal,
-  ModalProvider,
-} from "../components/manager/create-user-modal/modal-context";
-import CreateUserModal from "../components/manager/create-user-modal/index";
->>>>>>> eefaa681c45cafb1040bc380046cb2ecd9299319
 
 const ManagerUsers: React.FC = () => {
   const { openModal } = useModal();
